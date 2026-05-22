217f97688699e6f9fe0afdc2330712429b570dbe
Safiya Sajad <safiyasajad@gmail.com>
2026-05-22 19:56:09 +0530

    Update meal_model.dart
    changing the variable type to allow the admin to add recipes

--- lib/models/meal_model.dart
+++ lib/models/meal_model.dart
@@ -2,11 +2,17 @@ class Meal {
   final int id;
   final String title;
   final String imageUrl;
+  final String? diet;
+  final int? calories;
+  final bool isAdminMeal;
 
   Meal({
     required this.id,
     required this.title,
     required this.imageUrl,
+    this.diet,
+    this.calories,
+    this.isAdminMeal = false,
   });
 
   factory Meal.fromMap(Map<String, dynamic> map) {
@@ -14,6 +20,17 @@ class Meal {
       id: map['id'],
       title: map['title'],
       imageUrl: 'https://spoonacular.com/recipeImages/${map['image']}',
+    );
+  }
+
+  factory Meal.fromFirestore(Map<String, dynamic> map) {
+    return Meal(
+      id: 0,
+      title: map['title'] ?? '',
+      imageUrl: map['imageUrl'] ?? '',
+      diet: map['diet'],
+      calories: (map['calories'] as num?)?.toInt(),
+      isAdminMeal: true,
     );
   }
 }
